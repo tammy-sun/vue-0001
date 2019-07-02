4c28a86cf3679febd71cf67dac70f1b830173520
tammy <2649031352@qq.com>
2019-07-02 17:50:17 +0800

    init project

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+<!--    注意，如果引入没反应，则最有可能的问题是babel-loader 不正确，需要重新引入-->
+    <div class="app-container">
+
+        <!-- 顶部 Header 区域 -->
+        <mt-header fixed title="黑马程序员·Vue项目"></mt-header>
+
+
+        <!-- 中间的 路由 router-view 区域 -->
+
+
+        <!-- 底部 Tabbar 区域 -->
+        <nav class="mui-bar mui-bar-tab">
+            <router-link class="mui-tab-item" to="/home">
+                <span class="mui-icon mui-icon-home"></span>
+                <span class="mui-tab-label">首页</span>
+            </router-link>
+            <router-link class="mui-tab-item" to="/member">
+                <span class="mui-icon mui-icon-contact"></span>
+                <span class="mui-tab-label">会员</span>
+            </router-link>
+            <router-link class="mui-tab-item" to="/shopcar">
+				<span class="mui-icon mui-icon-extra mui-icon-extra-cart">
+					<span class="mui-badge">0</span>
+				</span>
+                <span class="mui-tab-label">购物车</span>
+            </router-link>
+            <router-link class="mui-tab-item" to="/search">
+                <span class="mui-icon mui-icon-search"></span>
+                <span class="mui-tab-label">搜索</span>
+            </router-link>
+        </nav>
+
+    </div>
+</template>
+
+<script>
+</script>
+
+
+<style  scoped>
+    .app-container {
+        padding-top: 40px;
+        overflow-x: hidden;
+    }
+</style>
---
+++ src/main.js
@@ -0,0 +1,18 @@
+//入口文件
+import Vue from  'vue'
+
+
+//按需导入Mint-UI 组件
+import { Header } from 'mint-ui'
+Vue.component(Header.name, Header)
+
+//导入MUI样式
+import './lib/mui/css/mui.min.css'
+//导入app组件
+
+import app from './App.vue'
+
+var vm = new Vue({
+    el: '#app',
+    render: c => c(app)
+})
